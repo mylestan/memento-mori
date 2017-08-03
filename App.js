0ec88f57d6ea9c33c4537291f26fa24b4a9c04a2
Myles Tan <mylestan7@gmail.com>
2017-08-02 23:40:07 -0400

    some cleanup

--- App.js
+++ App.js
@@ -16,14 +16,12 @@ class MementoMoriApp extends Component {
     return (
       <View style={styles.splashContainer}>
         <Image source={graphic} style={styles.graphic}/>
-        <View style={styles.aboutContainer}>
-          <View>
-            <Text style={styles.aboutText}>Memento Mori is a latin expression which translates into "remember that you have to die", or more simply "remember death". It, as well as skull and bone imagery, have been used since Ancient Rome as a reminder of one's own mortality, and offer a reminder to contemplate this regularly.</Text>
-            <Text style={styles.aboutText}>You can learn more about the origins of the phrase here: https://en.wikipedia.org/wiki/Memento_mori</Text>
-          </View>
-          <View>
-            <Text style={styles.footerText}>This app is a simple React Native learning project by Myles Tan. Code is online and open-source. Follow me on github @tanmyles. Made with love in the 6ix.</Text>
-          </View>
+        <View>
+          <Text style={styles.aboutText}>Memento Mori is a latin expression which translates into "remember that you have to die", or more simply "remember death". It, as well as skull and bone imagery, have been used since Ancient Rome as a reminder of one's own mortality, and offer a reminder to contemplate this regularly.</Text>
+          <Text style={styles.aboutText}>You can learn more about the origins of the phrase here: https://en.wikipedia.org/wiki/Memento_mori</Text>
+        </View>
+        <View>
+          <Text style={styles.footerText}>This app is a simple React Native learning project by Myles Tan. Code is online and open-source. Follow me on github @tanmyles. Made with love in the 6ix.</Text>
         </View>
       </View>
     );
@@ -33,40 +31,29 @@ class MementoMoriApp extends Component {
 const styles = StyleSheet.create({
   splashContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000',
-  },
-  graphic: {
-    width: 200,
-    height: 200
-  },
-  tagline: {
-    fontSize: 20,
-    color: '#fff',
-    textAlign: 'center',
-    marginTop: 100,
-    margin: 10
-  },
-  aboutContainer: {
-    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#000',
     padding: 20,
   },
-  aboutTitle: {
-    color: '#fff',
-    fontSize: 18,
+  graphic: {
+    width: 200,
+    height: 200,
+    marginTop: 50,
+    marginBottom: 50,
   },
   aboutText: {
     color: '#fff',
     fontSize: 16,
     marginBottom: 20,
+    textAlign: 'center',
   },
   footerText: {
     color: '#999',
     fontSize: 12,
     marginBottom: 5,
+    textAlign: 'center',
   },
 });
 
