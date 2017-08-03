580ff093f2f0e3997ffa3c803904dfa72d37520b
Myles Tan <mylestan7@gmail.com>
2017-08-02 23:08:00 -0400

    remove navigation - single page!

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -13,69 +7,28 @@ import {
   Image,
   Button
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
 
-class HomeScreen extends Component {
-  static navigationOptions = {
-    header: {
-      visible: false,
-      style: {
-        backgroundColor: '#000',
-      },
-    },
-  };
+class MementoMoriApp extends Component {
   render() {
-    const { navigate } = this.props.navigation;
     let graphic = {
       uri: 'https://images.genius.com/058b6018520093618e4c4d249e31bc35.794x794x1.jpg'
     };
     return (
       <View style={styles.splashContainer}>
         <Image source={graphic} style={styles.graphic}/>
-        <Button
-          color='#333'
-          accessibilityLabel='About'
-          onPress={() => navigate('About')}
-          title="About"
-        />
-      </View>
-    );
-  };
-}
-
-class AboutScreen extends Component {
-  static navigationOptions = {
-    title: 'About',
-    header: {
-      style: {
-        backgroundColor: '#000',
-      },
-      titleStyle: {
-        color: '#fff',
-      },
-      tintColor: '#fff',
-      backTitle: 'Back',      
-    },
-  };
-  render() {
-    return (
-      <View style={styles.aboutContainer}>
-        <View>
-          <Text style={styles.aboutText}>Memento Mori is a latin expression which translates into "remember that you have to die", or more simply "remember death". It, as well as skull and bone imagery, have been used since Ancient Rome as a reminder of one's own mortality, and offer a reminder to contemplate this regularly.</Text>
-          <Text style={styles.aboutText}>You can learn more about the origins of the phrase here: https://en.wikipedia.org/wiki/Memento_mori</Text>
-        </View>
-        <View>
-          <Text style={styles.footerText}>This app is a simple React Native learning project by Myles Tan. Code is online and open-source. Follow me on github @tanmyles. Made with love in the 6ix.</Text>
+        <View style={styles.aboutContainer}>
+          <View>
+            <Text style={styles.aboutText}>Memento Mori is a latin expression which translates into "remember that you have to die", or more simply "remember death". It, as well as skull and bone imagery, have been used since Ancient Rome as a reminder of one's own mortality, and offer a reminder to contemplate this regularly.</Text>
+            <Text style={styles.aboutText}>You can learn more about the origins of the phrase here: https://en.wikipedia.org/wiki/Memento_mori</Text>
+          </View>
+          <View>
+            <Text style={styles.footerText}>This app is a simple React Native learning project by Myles Tan. Code is online and open-source. Follow me on github @tanmyles. Made with love in the 6ix.</Text>
+          </View>
         </View>
       </View>
     );
   };
 }
-
-const MementoMori = StackNavigator({
-  Home: { screen: HomeScreen },
-  About: { screen: AboutScreen },
-});
 
 const styles = StyleSheet.create({
   splashContainer: {
@@ -117,4 +70,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('MementoMori', () => MementoMori);
+AppRegistry.registerComponent('MementoMori', () => MementoMoriApp);
